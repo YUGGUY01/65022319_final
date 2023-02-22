5e6ca524b69d919c3bd55ab90cc8306d87b07a01
YUGGUY01 <119989539+YUGGUY01@users.noreply.github.com>
2023-02-22 14:56:50 +0700

    Final 65022319

---
+++ test.js
@@ -0,0 +1,21 @@
+let TBox = document.getElementById('txtBox');
+let TwBox = document.getElementById('txtBox2');
+let ThrBox = document.getElementById('txtBox3');
+let menu = document.getElementById('menu');
+
+
+function TxtChange(){
+    let Textvalue = document.getElementById('txtBox').value;
+    ShowTxt.innerHTML = Textvalue;
+}
+function TxtChange(){
+    let Textvalue = document.getElementById('txtBox2').value;
+    ShowTxt.innerHTML = Textvalue;
+}
+
+function TxtChange(){
+    let Textvalue = document.getElementById('txtBox3').value;
+    ShowTxt.innerHTML = Textvalue;
+}
+
+alert("ข้อมูลไม่ครบ")
